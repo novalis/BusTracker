1d80c197e9a768c5c2cb9a546a3e2310befe214c
David Turner <novalis@openplans.org>
2009-04-23 22:02:58 -0400

    initial work on bus route import

---
+++ tracker/management/commands/create_bus_route.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# Imports tiger data from the table provided on the command-line into the tracker tables.
+
+from django.core.management.base import BaseCommand
+from optparse import make_option, OptionParser
+from tracker.models import *
+
+
+class Command(BaseCommand):
+    help = "Creates bus routes from a human-readable format."
+
+    def handle(self, bus_route_file, **kw):
+        f = open(bus_route_file)
+
+
+        for line in f:
+            if "#" in line:
+                line = line[:line.index("#")]
+
+            line = line.strip()
+            if not line:
+                continue
+
+            items = line.split(", ")
+            segment = {}
+            for item in items:
+                key, value = item.split(": ")
+                segment[key.lower()] = value
+
+            if 'route' in segment:
+                name = segment['route']
+                route = Route(name = name)
+                if name.startswith("M"):
+                    borough = ", Manhattan"
+
+                continue
+
+            road_name = segment['on']
+            road = Road.objects.get(name=road_name + borough)
+            if 'gid' in segment:
+                #some roadsegments must be chosen by GID because the
+                #roadsegment ordering system does not work for circles
+                segment = road.roadsegment_set.get(gid=int(segment['gid']))
+                RouteSegment(roadsegment = segment, route=route).save()
+            else:
+                from_road = Road.objects.get(name=segment['from'] + borough)
+                to_road = Road.objects.get(name=segment['to'] + borough)
+
+                for segment in road.roadsegment_set.all():
+                    if segment.geometry.intersects(from_road.geometry):
+                        from_order = segment.path_order
+                    if segment.geometry.intersects(to_road.geometry):
+                        to_order = segment.path_order
+                if from_order > to_order:
+                    from_order, to_order = to_order, from_order
+                
+                for segment in road.roadsegment_set.filter(path_order__gte=from_order, path_order__lt=to_order):
+                    route_segment = RouteSegment(route=route, roadsegment=segment)
+                    route_segment.save()
+
+#todo: update route geometry
