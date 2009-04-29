fd01275976656918a4d576d1a800a4d28cae6856
Nicholas Bergson-Shilcock <nicholasbs@madison.local>
2009-04-29 10:56:17 -0400

    update demo map with simple ui for loading in bus kml data

--- tracker/static/js/demo_map.js
+++ tracker/static/js/demo_map.js
@@ -6,7 +6,7 @@ function createMap(map_id) {
         maxResolution: 156543.03390625,
         numZoomLevels: 20,
     };
-    var map = new OpenLayers.Map('map', options);
+    map = new OpenLayers.Map('map', options);
     var baseMap = new OpenLayers.Layer.WMS(
         'OpenStreetMap',
         'http://demo.opengeo.org/geoserver_openstreetmap/gwc/service/wms',
@@ -15,9 +15,27 @@ function createMap(map_id) {
             format: 'image/png',
         }
     );
+
     map.addLayers([baseMap]);
     var center = new OpenLayers.LonLat(-73.99, 40.76); // Center of the universe
     center.transform(new OpenLayers.Projection('EPSG:4326'), map.getProjectionObject());
     map.setCenter(center, 14);
+
+    $('#load-btn').click(function() {
+        var bus_id = $('#bus-id').get(0).value;
+        var kmlUrl = 'kml?bus_id=' + bus_id;
+        console.log(kmlUrl);
+        loadBusKml(kmlUrl, 'bus ' + bus_id);
+    });
 }
 
+function loadBusKml(kmlUrl, name) {
+    var layerOptions = {
+        format: OpenLayers.Format.KML,
+        projection: new OpenLayers.Projection('EPSG:4326'),
+    };
+    var layer = new OpenLayers.Layer.GML(name, kmlUrl, layerOptions);
+    map.addLayer(layer);
+
+    return layer;
+}
