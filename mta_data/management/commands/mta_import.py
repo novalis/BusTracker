c38b6fb91ed61f20c808f5c16ec1e77efe3231e0
David Turner <novalis@openplans.org>
2009-05-12 17:36:09 -0400

    make dump work

--- mta_data/management/commands/mta_import.py
+++ mta_data/management/commands/mta_import.py
@@ -7,7 +7,8 @@ import re
 import sys
 
 #stif.bx0012sb.309113.wkd.closed
-filename_re = re.compile("stif.(?P<borough>m|b|bx|q|s)_?(?P<route_no>\d+)(?P<unknown_flags>x_|c_|o_|sb|__).(?P<unknown_2>\w{6}).(?P<day_of_week>sat|sun|wkd)")
+#stif.q_0031__.509084.sat
+filename_re = re.compile("stif\\.(?P<borough>m|b|bx|q|s)_?(?P<route_no>\d{4})(?P<unknown_flags>x_|c_|o_|sb|__)\\.(?P<unknown_2>\w{6})\\.(?P<day_of_week>sat|sun|wkd\\.open|wkd.\\closed)")
 
 class FieldDef:    
     def __init__(self, name, width, numeric = False, hex = False, stripped = True, strip_underscore=False):
@@ -15,8 +16,11 @@ class FieldDef:
         self.width = width
         self.stripped = stripped
         self.strip_underscore = strip_underscore
-        self.numeric = numeric or hex
+        self.numeric = numeric
         self.hex = hex
+
+    def __repr__(self):
+        return "<FieldDef ('%s', %d)>" % (self.name, self.width)
 
 class LineFormat:
     def __init__(self, *fields):
@@ -31,19 +35,23 @@ class LineFormat:
         pos = 0
         for field in self.fields:
             try:
-                values[field.name] = dataline[pos:pos + field.width]
+                value = dataline[pos:pos + field.width]
                 if field.stripped:
-                    values[field.name] = values[field.name].strip()
+                    value = value.strip()
                 if field.strip_underscore:
-                    values[field.name] = values[field.name].strip("_")
+                    value = value.strip("_")
                 if field.numeric:
-                    if field.hex:
-                        values[field.name] = int(values[field.name], 16)
+                    if not value:
+                        value = -1
+                    elif field.hex:
+                        int(value, 16) #don't actually convert to an int, but assert that we can do so
                     else:
-                        values[field.name] = int(values[field.name])
+                        value = int(value)
+                values[field.name] = value
             except:
                 print >>sys.stderr, "Error parsing line %s\nat field %s, at pos %d" % (dataline, field.name, pos)
                 print >>sys.stderr, "Parsed so far: %s" % pformat(values)
+                import pdb;pdb.set_trace()
                 raise
             pos += field.width
         return values
@@ -58,7 +66,7 @@ route_format = LineFormat(
     FieldDef('route_id', 6),
     FieldDef('borough', 2),
     FieldDef('route_no', 5),
-    FieldDef('UNKNOWN_1', 2, numeric=True),
+    FieldDef('UNKNOWN_1', 2),
     FieldDef('depot', 24), 
     FieldDef('street_name', 24), 
     FieldDef('UNKNOWN_2', 7),  #this appears to be the same as unknown_2 in the filename
@@ -81,12 +89,12 @@ stop_format = LineFormat(
     FieldDef('stop_id', 4, hex=True),
     FieldDef('street1', 22),
     FieldDef('street2', 22),
-    FieldDef('UNKNOWN_2', 11, numeric=True),
-    FieldDef('borough', 2),
-    FieldDef('UNKNOWN_3', 10), #optional, and it appears to correlate to location, as in Bartel PRichard SQ -> BPRSQ 
+    FieldDef('UNKNOWN_2', 12, numeric=True),
+    FieldDef('borough', 3),
+    FieldDef('UNKNOWN_3', 9), #optional, and it appears to correlate to location, as in Bartel PRichard SQ -> BPRSQ 
     FieldDef('stop_type', 4), #N or D for Normal or Depot (?)
-    FieldDef('latitude', 9, numeric=True),
-    FieldDef('longitude', 9, numeric=True),
+    FieldDef('latitude', 10, numeric=True),
+    FieldDef('longitude', 10, numeric=True),
     FieldDef('UNKNOWN_4', 6, numeric=True)
 )
 
@@ -99,6 +107,8 @@ stop_format = LineFormat(
 #012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012
 
 #212e1600034500N 1 ad1 00037500         4BX07_0019     BX07                        R3070 N N                6880 KB    8743044      4   BX07 00039000   15   BX07      4   BX07 00032200
+#2154cd00006000W 1 15ec00008900         1S4898_0032   S4898                        R6481 N N                6760 CA    9012496      1  S4898 00014000   51    S48      1  S4898 00004000
+#21f9d 00031500W 1447c100034700       201SBS12_0037   SBS12                        S8127 N N                7270 GH    8745788    201  SBS12 00036000   13  SBS12    201  SBS12 00031300
 
 trip_format = LineFormat(
     FieldDef('UNKNOWN_0', 2, hex=True),
@@ -108,12 +118,12 @@ trip_format = LineFormat(
     FieldDef('direction', 2), #NSEW only, I think.
     FieldDef('UNKNOWN_2', 2, numeric=True), 
     FieldDef('UNKNOWN_3', 3, hex=True), 
-    FieldDef('UNKNOWN_4', 9, numeric=True),  
+    FieldDef('UNKNOWN_4', 9, hex=True),  
     FieldDef('UNKNOWN_5', 9),  #I only hypothesize that this field is not part of the prev or next
     FieldDef('UNKNOWN_6', 1, numeric = True),
     FieldDef('route_name', 5, strip_underscore=True),
-    FieldDef('UNKNOWN_7', 4, numeric = True), 
-    FieldDef('UNKNOWN_8', 4), #another hypothetical-dark-matter-type field
+    FieldDef('UNKNOWN_7', 5, numeric = True, strip_underscore=True), 
+    FieldDef('UNKNOWN_8', 3), #another hypothetical-dark-matter-type field
     FieldDef('route_name_again_1', 5),
     FieldDef('UNKNOWN_9', 24), 
     FieldDef('UNKNOWN_10', 1),
@@ -124,15 +134,17 @@ trip_format = LineFormat(
     FieldDef('UNKNOWN_15', 5, numeric = True),
     FieldDef('UNKNOWN_16', 7),
     FieldDef('UNKNOWN_17', 7, numeric = True),
-    FieldDef('UNKNOWN_18', 7, numeric = True),
-    FieldDef('UNKNOWN_19', 3), #dark matter
+    FieldDef('UNKNOWN_18', 5),
+    FieldDef('UNKNOWN_19', 2, numeric = True),
+    FieldDef('UNKNOWN_20', 3), #dark matter
     FieldDef('route_name_again_2', 5),
-    FieldDef('UNKNOWN_20', 9, numeric = True),
-    FieldDef('UNKNOWN_21', 7, numeric = True),
+    FieldDef('UNKNOWN_21', 9, numeric = True),
+    FieldDef('UNKNOWN_22', 6, numeric = True),
     FieldDef('route_name_again_3', 5),
-    FieldDef('UNKNOWN_22', 8, numeric = True),
+    FieldDef('UNKNOWN_23', 6),
+    FieldDef('UNKNOWN_24', 3, numeric = True),
     FieldDef('route_name_again_4', 5),
-    FieldDef('UNKNOWN_23', 8, numeric = True),
+    FieldDef('UNKNOWN_25', 9, numeric = True),
 )
 
 
@@ -153,19 +165,31 @@ tripstop_format = LineFormat(
 
 #012345678901234567890123456789012345678901
 #351060    M06       MIDTOWN 59 ST via 6 AV
-routename_format = LineFormat(
+
+headsign_format = LineFormat(
     FieldDef("UNKNOWN_1", 2, numeric=True), 
-    FieldDef("headsign_id", 6, numeric=True), 
-    FieldDef("route_id", 4), 
+    FieldDef("headsign_id", 5, numeric=True), 
+    FieldDef("UNKNOWN_2", 3),
+    FieldDef("route_id", 5), 
+    FieldDef("UNKNOWN_3", 5), 
     FieldDef("headsign", 100),
 )
 
-def parse_schedule_file(filename):
+def parse_schedule_file(path):
     """Returns a dict with general data about the route, as well as stops and trips, and stops for each trip"""
+    filename = os.path.basename(path)
     match = filename_re.match(filename)
+
+    if not match:
+        print filename
+        import pdb;pdb.set_trace()
     route = match.groupdict()
-    f = open(filename)
-    route.update(route_format.parse(f.readline()))
+
+    route['original_filename'] = filename
+
+    f = open(path)
+    line = f.readline().strip('\r\n')
+    route.update(route_format.parse(line))
 
     route['stops'] = []
     route['trips'] = []
@@ -174,6 +198,7 @@ def parse_schedule_file(filename):
     trip = None
 
     for line in f.readlines():
+        line = line.strip('\r\n')
         if len(line) == stop_format.width:
             stop = stop_format.parse(line)
             route['stops'].append(stop) #stops are out-of-order, and we don't yet know how to order them
@@ -187,6 +212,8 @@ def parse_schedule_file(filename):
             trip['stops'].append(stop) 
         elif len(line) == headsign_format.width:
             route['headsigns'].append(headsign_format.parse(line))
+        elif len(line) == len("41000026000078000849000066000002"):
+            route['more_unknown_crap'] = line
         else:
             print "unknown line %s" % line
 
@@ -196,18 +223,28 @@ holidays = {'Christmas' : 'xmd', 'Christmas Eve' : 'xme',
             'New Year\'s Day' : 'nyd', 'New Year\'s Eve' : 'nye'}
 
 class Command(BaseCommand):
-    def handle(self, dirname, **kw):
-        routes = {}
+    def handle(self, dirname, outdir, **kw):
         files = os.listdir(dirname)
+
+        def store(route):
+            filename = "%s%s.%s.%s.json" % (route['borough'], route['route_no'], route.get('holiday', 'normal'), route['day_of_week'])
+            path = os.path.join(outdir, filename)
+            if os.path.exists(path):
+                import pdb;pdb.set_trace()
+            out = open(path, "w")
+            print >>out, dumps(route)
+            out.close()
+
         for filename in files:
             if filename.startswith('stif'):
                 route = parse_schedule_file(os.path.join(dirname, filename))
-            if filename in holidays:
+                store(route)
+            elif filename in holidays:
                 holiday_abbrev = holidays[filename]
                 subdirname = os.path.join(dirname, filename)
                 files = os.listdir(subdirname)
                 for filename in files:
-                    route = parse_schedule_file(os.path.join(subdirname, filename))
-                    route['day_of_week'] = holiday_abbrev
-        routes.append(route)
-        print dumps(routes)
+                    if filename.startswith('stif'):
+                        route = parse_schedule_file(os.path.join(subdirname, filename))
+                        route['holiday'] = holiday_abbrev
+                        store(route)
