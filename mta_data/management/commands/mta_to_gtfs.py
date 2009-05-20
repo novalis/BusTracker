99ef27283fbc162db9401fe1302a9a0b6d0a58ee
David Turner <novalis@openplans.org>
2009-05-20 19:22:28 -0400

    numerous fixes for gtfs importer

--- mta_data/management/commands/mta_to_gtfs.py
+++ mta_data/management/commands/mta_to_gtfs.py
@@ -13,7 +13,27 @@ def google_time_from_centiminutes(centiminutes):
     seconds = ((centiminutes % 6000) - minutes * 100) * 60 / 100
     return "%02d:%02d:%02d" % (hours, minutes, seconds)
 
+rename_location = {
+'BROADWAY at 207 ST' : 'BROADWAY at W 207 ST',
+'NARROWS ROAD S at FINGERBOARD ROAD' : 'NARROWS RD S at FINGERBOARD RD',
+'NARROWS RD S at FINGERBOARD ROAD' : 'NARROWS RD S at FINGERBOARD RD',
+'NARROWS ROAD S at FINGERBOARD RD' : 'NARROWS RD S at FINGERBOARD RD',
+}
 
+def route_for_trip(feed, name, trip_rec, headsign):
+    route_id = trip_rec['headsign_id']
+
+    if route_id in feed.routes:
+        return feed.routes[route_id]
+
+    #create the route
+    long_name = headsign + ' ' + trip_rec['direction']
+    route = transitfeed.Route(route_id=route_id,
+                             short_name=name, 
+                             long_name=long_name,
+                             route_type="Bus")
+    feed.AddRouteObject(route)
+    return route
 
 class Command(BaseCommand):
     """Import mta schedule and route data into DB.  Assume route data is 
@@ -42,26 +62,24 @@ class Command(BaseCommand):
                     print "Don't know how to handle loop routes yet"
                     continue
 
-
-                #fixme: split multiroutes
-
-                #add the route itself
                 long_name = route_rec["street_name"]
                 if long_name.startswith(name):
                     long_name = long_name[len(name):].strip()
-                route = feed.AddRoute(short_name=name, 
-                                      long_name=long_name,
-                                      route_type="Bus")
 
 
+                #fixme: split multiroutes
+                    
                 period = feed.GetServicePeriod(route_rec['day_of_week'].upper())
 
                 stop_hexid_to_stop = {}
                 #add all stops
                 for stop_rec in route_rec['stops']:
                     location = "%s at %s" % (stop_rec['street1'], stop_rec['street2'])
+
+                    location = rename_location.get(location, location)
+
                     #check for duplicate box ids
-                    box_no = stop_rec['box_no']
+                    box_no = str(stop_rec['box_no'])
                     if location in stop_name_to_id:
                         box_no = stop_name_to_id[location]
                     else:
@@ -72,8 +90,16 @@ class Command(BaseCommand):
                     lat = stop_rec['latitude'] / 1000000.0
                     lng = stop_rec['longitude'] / 1000000.0
 
+                    #not in NYC area
+                    if not (-72 > lng > -75) or not (41 > lat > 39):
+                        print "bad lat, lng", lat, lng
+                        import pdb;pdb.set_trace()
+                        
+
                     nearest = feed.GetNearestStops(lat, lng, 1)
-                    if len(nearest):
+                    if len(nearest) and not ' LANE ' in nearest[0].stop_name:
+                        #sometimes bus stops really are like 1 m away because 
+                        #they are two lanes in a multilane terminal area
                         nearest = nearest[0]
                     else:
                         nearest = None
@@ -100,7 +126,10 @@ class Command(BaseCommand):
                         #these trips are bogus -- their stops are out-of-order.
                         continue
                     hid = trip_rec['headsign_id']
-                    headsign = headsigns.get(hid)
+                    headsign = headsigns.get(hid, long_name)
+
+                    route = route_for_trip(feed, name, trip_rec, headsign)
+
                     trip = route.AddTrip(feed, 
                                          headsign, 
                                          service_period=period)
@@ -110,7 +139,7 @@ class Command(BaseCommand):
                         trip.AddStopTime(stop_hexid_to_stop[stop_id], 
                                          stop_time=stop_time)
 
-            feed.Validate()
+                feed.Validate()
             feed.WriteGoogleTransitFeed('mta_data/out.zip')
         except Exception, e:
             import traceback
